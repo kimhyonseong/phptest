8f404707df2818addbad8c1f51fd75711b1c7615
kimhyonseong <corea6524@gmail.com>
2018-10-18 15:58:57 +0900

    two

--- php/log.php
+++ php/log.php
@@ -6,5 +6,9 @@
      * Time: 오후 2:06
      */
     $id =$_POST["id"];
-    echo $id;
+    $pw=$_POST["pw"];
+    if ($id=="Red" && $pw =="1234") {
+        echo "LogIn success <br>";
+    }
+    else echo "LogIn Fail<br>";
 ?>
--- php/test.php
+++ php/test.php
@@ -5,11 +5,34 @@
  * Date: 2018-10-11
  * Time: 오후 8:11
  */
-$a = 10; $b = 20;
+define("ROOT_LOCATION","/usr/local/www");
+$directory = ROOT_LOCATION;
+echo $directory,"<br>";
+$a = 10; $b = 20; $c="호랑이"; $d="나비";
 $oxo=array(array("x",'o','x'),
             array('o','x','o'),
             array('x','o','x'));
 echo $oxo[0][0]; echo "<br>";
-echo "$a + $b = " , $a+$b , "<br>";
-echo "asd" . $a ."<br>";
+echo "$a + $b = " , $a+$b , "<br>";  //double quotes
+    echo "asd\n" . $a.=$b ."<br>"; //No , use .-> can't calculate
+//echo "$a + $b = " . $a+$b; error
+//print "$a + $b = " . $a+$b; error
+    echo '$a + $b = '. '$a+$b'.'<br>';  //single quotes
+    echo "\nDate\tName\tPayment\rK"."<br>";  //\n \t \r
+
+    $out= <<<_END
+<br>Debugging is twice as hard as writing the code in the first place.
+<br>Therefor, if you write the code as cleverly as possible, you are,
+by definition, not smart enough to debug "it".
+<br><br>
+- Brian W. Kernighan<br>
+_END;
+//If no write <br>, Don't Line break
+    echo $out;
+    $number = 12345 * 67890;
+    echo "number = $number ".substr($number,3,4)."<br>";
+    $pi="3.14"; $radius=5;
+    echo $pi * ($radius * $radius),"<br>";
+    echo "This is Line ". __Line__ . " of file " . __File__."<br>";
+    echo "Namespace " . __NAMESPACE__;
 ?>
