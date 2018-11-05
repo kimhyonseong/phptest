8415a9cac26ff1cc01d38d5d0e3c9ccbc83cd45d
kimhyonseong <corea6524@gmail.com>
2018-11-05 22:11:41 +0900

    nine

--- php/member.php
+++ php/member.php
@@ -1,6 +1,4 @@
-<html>
-<head><meta http-equiv="Content-Type" content="text/html; charset=utf-8"></head>
-<body>
+
     <?php
         /**
          * Created by PhpStorm.
@@ -8,7 +6,7 @@
          * Date: 2018-11-05
          * Time: 오후 3:30
          */
-    //require_once 'member.php';
+    require_once 'member.php';
     $id = $_POST['아이디'];  //name이 들어감
     $pw = $_POST['비밀번호'];
     $email = $_POST['이메일'];
@@ -18,11 +16,25 @@
     $con = mysqli_connect("localhost","root","","sign");
     //$mysqli = new mysqli('localhost','root','','sign');
     //mysqli_select_db($mysqli,"imf");
+
+    mysqli_query($con,"set session character_set_connection=utf8;");
+    mysqli_query($con,"set session character_set_results=utf8;");
+    mysqli_query($con,"set session character_set_client=utf8;");
+
     if ($con->connect_error) die($con->connect_error);
 
-    if(isset($id)&&isset($name)){
-        
+    if(isset($id)&&
+        isset($pw)&&
+        isset($name)&&
+        isset($email)&&
+        isset($phone)    )
+    {
+        //if() //id 같으면 추가 못하게
+        $query = "INSERT INTO imf(id,pw,name,email,phone) VALUES('$id','$pw','$name','$email','$phone')";
+        $result = $con->query($query);
+        echo $name ."님 정보가 추가 되었습니다.";
     }
+    if(!$result) die("Database access fail: ".$con->error);
+
     ?>
-</body>
-</html>
+
