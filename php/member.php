961284dbdf32e71456006d1e7d8264eedc8eb8c8
kimhyonseong <corea6524@gmail.com>
2018-11-07 14:57:00 +0900

    11

--- php/member.php
+++ php/member.php
@@ -7,6 +7,7 @@
          * Time: 오후 3:30
          */
     require_once 'member.php';
+
     $id = $_POST['아이디'];  //name이 들어감
     $pw = $_POST['비밀번호'];
     $email = $_POST['이메일'];
@@ -23,18 +24,32 @@
 
     if ($con->connect_error) die($con->connect_error);
 
-    if(isset($id)&&
-        isset($pw)&&
-        isset($name)&&
-        isset($email)&&
-        isset($phone)    )
+    function mysql_fix_string($con, $string)
     {
-        //if() //id 같으면 추가 못하게
-        $query = "INSERT INTO imf(id,pw,name,email,phone) VALUES('$id','$pw','$name','$email','$phone')";
-        $result = $con->query($query);
-        echo $name ."님 정보가 추가 되었습니다.";
+        if(get_magic_quotes_gpc()) $string = stripcslashes($string);
+        return mysqli_real_escape_string($con,$string);
     }
-    if(!$result) die("Database access fail: ".$con->error);
+    $query1 ="SELECT * FROM imf WHERE id = \"$id\"";
+    $check = $con->query($query1);
+    $check->data_seek(0);
+    $real_ID = $check->fetch_assoc()['id'];
+    if($id==NULL || $name==NULL || $email==NULL || $phone==NULL || $pw==NULL) echo "빈칸을 채워주세요.<br>";
+    else if(strpos($id," ")!==false) echo "ID에 공백을 제거해주세요.<br>";
+    else if(strpos($name," ")!==false) echo "이름에 공백을 제거해주세요.<br>";
+    else if(isset($id)&&isset($pw)&&isset($name)&&isset($email)&&isset($phone)    )
+    {
+        if($real_ID == $id) //id 같으면 추가 못하게
+        {
+            echo "이미 가입되어있는 아이디입니다.";
+        }
+        else {
+            $query1 = "INSERT INTO imf(id,pw,name,email,phone) VALUES(\"$id\",\"$pw\",\"$name\",\"$email\",\"$phone\")";
+            $result = $con->query($query1);
+
+            echo $name . "님, 가입이 완료되었습니다.<br>";
+        }
+    }
+    //if(!$result) die("Database access fail: ".$con->error);
 
     ?>
 
--- php/sign.php
+++ php/sign.php
@@ -9,37 +9,37 @@
     <h1>회원가입</h1>
     <p>start</p>
     <form name="join" method="POST" action="member.php">
-    <table border="0" cellpadding="2" cellspacing="2">
+    <table border="0" cellpadding="2" cellspacing="2" style="width: 350px; height: 100px; margin: auto; ">
         <tr>
-            <td width="100px" align="center" valign="middle">아이디</td>
+            <td width="100px" align="center" valign="middle">ID</td>
             <td>
-                <input type="text" size="30" name="아이디" title="id">
+                <input type="text" size="20" name="아이디" title="id">
             </td>
-            <td><form name="check" method="post" action="query.php"><input type="submit" title="확인" id="중복확인1" value="중복확인"></form></td>
+            <td><input type="button" title="확인" id="중복확인1" value="중복확인"></td>
         </tr>
 
         <tr>
             <td width="100px" align="center" valign="middle">비밀번호</td>
             <td>
-                <input type="text" size="30" name="비밀번호" title="PW">
+                <input type="text" size="20" name="비밀번호" title="PW">
             </td>
         </tr>
         <tr>
             <td width="100px" align="center" valign="middle">이름</td>
             <td>
-                <input type="text" size="30" name="이름" title="name">
+                <input type="text" size="20" name="이름" title="name">
             </td>
         </tr>
         <tr>
             <td width="100px" align="center" valign="middle">E-mail</td>
             <td>
-                <input type="text" size="30" name="이메일" title="email">
+                <input type="text" size="20" name="이메일" title="email">
             </td>
         </tr>
         <tr>
             <td width="100px" align="center" valign="middle">전화번호</td>
             <td>
-                <input type="text" size="30" name="전번" title="phone">
+                <input type="text" size="20" name="전번" title="phone"">
             </td>
         </tr>
     </table>
