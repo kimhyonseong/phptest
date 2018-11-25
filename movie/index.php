b756bb28d16d076212d7530da1c564de1fecb52f
kimhyonseong <corea6524@gmail.com>
2018-11-25 15:36:31 +0900

    movie form

---
+++ movie/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+    <meta charset="UTF-8">
+    <title>Home</title>
+    <link rel="stylesheet" href="http://localhost/firststorm/movie/hsmovie.css">
+</head>
+<body>
+<ul id="menu">
+    <li><a href="http://localhost/firststorm/movie/login/" >로그인</a></li>
+    <li><a href="http://localhost/firststorm/movie/join/" >회원가입</a></li>
+</ul>
+<header><a href="http://localhost/firststorm/movie/">HS Movie</a></header>
+<nav>
+    <ul>
+        <li><a href="http://localhost/firststorm/movie/ticket/">예매</a></li>
+        <li><a href="http://localhost/firststorm/movie/movies/">영화</a></li>
+        <li><a href="http://localhost/firststorm/movie/theater/">극장</a></li>
+        <li><a href="http://localhost/firststorm/movie/event/">이벤트</a></li>
+    </ul>
+</nav>
+<article>
+    <h2>Event</h2>
+    <img src="picture/free.jpg" height="200" width="300">
+    <h2>Movie section</h2>
+    <a href="" ><img src="picture/culseo.jpg" height="200px" width="140px"></a>
+    <a href="" ><img src="picture/sing.jpg" height="200px" width="140px"></a>
+    <a href="" ><img src="picture/back.jpg" height="200px" width="140px"></a>
+    <h2>공지사항</h2>
+    <a href="" >영화 가격표 안내</a>
+</article>
+</body>
+</html>
