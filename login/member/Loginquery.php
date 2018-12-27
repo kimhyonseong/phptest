77a351c0e90627397c33cf97c583006bc19996a2
kimhyonseong <corea6524@gmail.com>
2018-12-27 23:59:02 +0900

    login ok

--- login/member/Loginquery.php
+++ login/member/Loginquery.php
@@ -13,7 +13,21 @@
     $real_id = mysqli_real_escape_string($con,$id);
     $real_pw = mysqli_real_escape_string($con,$pw);
 
-    $Login_query = "select id,pw from imf";
+    $Login_query = "select id,pw from imf where id='$real_id'";
     //관계배열로 아이디랑 패스워드 묶어서 확인하고 로그인하기
+    $Login_result = mysqli_query($con,$Login_query);
+    $Login = mysqli_fetch_array($Login_result);
+    $Login_id = $Login['id'];
+    $Login_pw = $Login['pw'];
 
+    if($real_id == $Login_id && $real_pw == $Login_pw)
+    {
+        //여기 뭘 써야될지 모르겠음;;
+    }
+
+    else
+    {
+        $msg = "아이디 또는 비밀번호를 확인해주세요.";
+        echo "<script>alert('$msg'); history.back(); </script>";
+    }
     ?>
