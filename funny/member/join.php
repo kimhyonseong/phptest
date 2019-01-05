ccff895094c9acf8645aa9f42c05c436878c0add
kimhyonseong <corea6524@gmail.com>
2019-01-05 15:20:05 +0900

    funny join

--- funny/member/join.php
+++ funny/member/join.php
@@ -16,7 +16,10 @@
     $E_mail = $real_Email1 . mysqli_real_escape_string($DB_connect,"@") . $real_Email2;
     $real_ID = mysqli_real_escape_string($DB_connect,$_POST['ID']);
 
-    if( $real_ID==null || $real_pwd==null || $real_Email1==null || $real_Email2==null || $real_pwd_ch==null || $real_name==null)
+    $ID_ch = mysqli_query($DB_connect,"SELECT ID FROM member WHERE ID = '$real_ID'");
+    $ch_result = mysqli_fetch_array($ID_ch);
+
+    if( $real_ID==null || $real_pwd==null || $real_Email1==null || $real_Email2==null || $real_pwd_ch==null || $real_name==null) # 비었을시
     {
         $msg = "빈칸을 채워주세요.";
         echo "<script>alert('$msg')</script>";
@@ -33,13 +36,16 @@
         $msg = "비밀번호와 비밀번호 확인이 일치하지 않습니다.";
         echo "<script>alert('$msg'); history.back();</script>";
     }
-    else if() # 아이디 같은거 확인
+    else if($real_ID == $ch_result['ID']) # 아이디 중복 확인
     {
-
+        $msg = "이미 사용중인 ID 입니다.";
+        echo "<script>alert('$msg'); history.back();</script>";
     }
 
     else
     {
-
+        $insert_query = "INSERT INTO member(ID,name,pwd,Email) VALUES('$real_ID','$real_name','$real_pwd','$E_mail')";
+        mysqli_query($DB_connect,$insert_query);
+        //header('Location: ../'); # 어디로 이동할지
     }
 ?>
