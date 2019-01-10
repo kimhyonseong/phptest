55a2f775452224fd7cb4e4d98b76260fd959b259
kimhyonseong <corea6524@gmail.com>
2019-01-10 21:09:19 +0900

    color search in DB

--- funny/content/content.php
+++ funny/content/content.php
@@ -7,5 +7,9 @@
      */
     include_once __DIR__.'/../DBconnect/DBconnect.php';
 
+    $key_word = mysqli_real_escape_string($DB_connect,$_GET['KeyWord']);
+    $search = "select * from color where name like '%$key_word%' or code like '%$key_word%' or strong like '%$key_word%'";
+    $result=mysqli_query($DB_connect,$search);
+    //$search_result = mysqli_fetch_array($result);
 
     ?>
--- funny/view/search.php
+++ funny/view/search.php
@@ -27,25 +27,20 @@
 <nav>
     <br>
     <form method="get" action="search.php">
-        <input type="text" name="KeyWord" placeholder="search"><input type="submit">
+        <input type="text" name="KeyWord" value="<?=$_GET['KeyWord']?>"><input type="submit">
     </form>
     <br>
 </nav>
 <article>
     <?php
-        $key_word = mysqli_real_escape_string($DB_connect,$_GET['KeyWord']);
-        $search = "select * from color where name like '%$key_word%' or code like '%$key_word%' or strong like '%$key_word%'";
-        $result=mysqli_query($DB_connect,$search);
-        $search_result = mysqli_fetch_array($result);
 
-        var_dump($search_result['0']);
-        var_dump($search_result['0']);
         while($search_result=mysqli_fetch_array($result)) {
-            /*echo '<div style="width: 200px; height: 300px; border: black 1px solid;">';
+            echo '<div style="width: 200px; height: 300px; border: black 1px solid; float: left; margin: 10px;">';
             echo '<div style="background-color: ' . $search_result['code'] . '; width: 150px; height: 200px;"></div>';
             echo '<div>' . $search_result['name'] . '</div>';
-            echo '</div>';*/
-            var_dump($search_result['0']);
+            echo '</div>';
+            //var_dump($search_result['0']);
+            //echo "<br>";
         }
     ?>
 </article>
