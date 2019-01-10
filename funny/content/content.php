f781480e5cf2ba8f93b8d40965df48d5d6fb7f3a
kimhyonseong <corea6524@gmail.com>
2019-01-10 20:30:17 +0900

    search error

---
+++ funny/content/content.php
@@ -0,0 +1,11 @@
+<?php
+    /**
+     * Created by PhpStorm.
+     * User: KimHyonSeong
+     * Date: 2019-01-10
+     * Time: 오후 6:30
+     */
+    include_once __DIR__.'/../DBconnect/DBconnect.php';
+
+
+    ?>
--- funny/view/search.php
+++ funny/view/search.php
@@ -21,6 +21,7 @@
             include_once __DIR__.'/header/Login_header.php';
         else
             include_once __DIR__.'/header/header.php';
+        include_once __DIR__.'/../content/content.php';
     ?>
 </header>
 <nav>
@@ -31,7 +32,22 @@
     <br>
 </nav>
 <article>
+    <?php
+        $key_word = mysqli_real_escape_string($DB_connect,$_GET['KeyWord']);
+        $search = "select * from color where name like '%$key_word%' or code like '%$key_word%' or strong like '%$key_word%'";
+        $result=mysqli_query($DB_connect,$search);
+        $search_result = mysqli_fetch_array($result);
 
+        var_dump($search_result['0']);
+        var_dump($search_result['0']);
+        while($search_result=mysqli_fetch_array($result)) {
+            /*echo '<div style="width: 200px; height: 300px; border: black 1px solid;">';
+            echo '<div style="background-color: ' . $search_result['code'] . '; width: 150px; height: 200px;"></div>';
+            echo '<div>' . $search_result['name'] . '</div>';
+            echo '</div>';*/
+            var_dump($search_result['0']);
+        }
+    ?>
 </article>
 </body>
 </html>
