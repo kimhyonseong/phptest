5a9733a709fd89a982e5ef59af76e62aa018da1b
kimhyonseong <corea6524@gmail.com>
2019-01-19 12:32:14 +0900

    change DB password

--- funny/DBconnect/DBconnect.php
+++ funny/DBconnect/DBconnect.php
@@ -5,5 +5,5 @@
      * Date: 2019-01-04
      * Time: 오후 6:53
      */
-    $DB_connect=mysqli_connect('localhost','root','','color');
+    $DB_connect=mysqli_connect('localhost','root','khs','color');
     ?>
